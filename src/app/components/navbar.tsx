40126d397dc466067f92f580d044883165715656
Yonaries <yonaries.dev@gmail.com>
2024-03-15 21:32:12 +0300

    fix: nav z index

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 export default function Navbar({}: Props) {
   return (
-    <div className='fixed top-0 z-10 flex h-fit w-full px-12 py-6 text-black'>
+    <div className='fixed top-0 z-50 flex h-fit w-full px-12 py-6 text-black'>
       <div className='flex w-full items-center justify-between rounded-full bg-white px-3 py-2 shadow-sm'>
         <div className='flex items-center gap-8 px-2'>
           <div className='text-2xl font-bold'>PeerConnect</div>
